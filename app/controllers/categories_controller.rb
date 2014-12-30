6c1926a2ea581c11b76c9f5c4f7013918576b1ff
Max Sveshnikov <msveshnikov@thumbtack.net>
2014-12-30 13:01:30 +0600

    first pleasant looking release!

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories
   def index
-    @categories = Category.all
+    @categories = Category.where("ParentCategory_ID = 0")
   end
 
   # GET /categories/1
