0639918ec292bab90eb77af979e396009b9d3b0b
Max Sveshnikov <msveshnikov@thumbtack.net>
2014-12-31 15:08:13 +0600

    ingredients!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+#TODO: ingridients
+#TODO: search for recipe and ingridient
+#TODO: breadcrumbs
+#TODO: +1 button
+#TODO: mobile site
+#TODO: ratings & votes
--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,7 +1,20 @@
 require 'json'
 
 module RecipesHelper
-  def array
+  def steps
     JSON.parse(@recipe.Media)["photos"]
+  end
+
+  def ingredients
+    a = JSON.parse(@recipe.Ingredients)
+    a.each do |e|
+      b = e["childs"]
+      b.each do |l|
+        l["type"] = Type.find(l["type"]).title unless l["type"]=="0"
+        l["type"]="" if l["type"]=="0"
+        l["id"] = Ingredient.find(l["id"]).Title unless l["id"]=="0"
+      end
+    end
+    return a
   end
 end
