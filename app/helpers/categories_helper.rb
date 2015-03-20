1142b75729b82b949eea6c332f2d8aa2731de777
Max Sveshnikov <msveshnikov@gmail.com>
2015-03-20 10:10:21 +0600

    convert images

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -3,6 +3,10 @@ module CategoriesHelper
     recipe=category.recipes.last
     recipe=category.keyword_recipes.first if category.keywords
     a=JSON.parse(recipe.Media) if recipe
-    return a["photos"].last["src_small"] if recipe
+    return url(a["photos"].last["src_small"]) if recipe
+  end
+
+  def url(s)
+    s.split('/').last
   end
 end
